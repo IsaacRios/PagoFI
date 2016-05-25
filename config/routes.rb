0a8e43ff09e506c3c9d1f5a7b46bb4ad0c1656fb
Isaac <rios.isaac@webmonster.mx>
2016-05-24 19:04:33 -0500

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'charges/index'
+
+  post 'charges/create'
+
+  get 'charges/show'
+
+  #get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'charges#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
